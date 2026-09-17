b3a66939376f7bc5527e2808836260536ae49f4c
Youssef Mostafa <youssefm32@nycstudents.net>
2026-09-17 12:38:13 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,45 @@
+/* let height = 15;
+
+
+function canRide(height){
+    if (height >= 48){
+        console.log("You can ride!")
+    }
+    else{
+        console.log("You are way too short!")
+    }
+}
+canRide(height); */
+
+
+/* const key = 4;
+
+switch (key) {
+  case 1:
+    console.log("Go to the park!");
+    break;
+  case 2:
+    console.log("Go to the beach or pool! Very hot!");
+    break;
+  case 3:
+    console.log("Go eat out with your friends at a resturaunt!");
+    break;
+  case 4:
+    console.log("Just stay home and play video games! Too cold!");
+    break;
+  default:
+    // the else essentially
+    console.log("your not on earth");
+} */
+
+
+
+//Combine && and ||: write a condition for a movie ticket discount that applies if the person is a student or a senior, and it's a weekday.
+
+const age = student
+const day = weekday
+
+if ((age === senior) || day === weekend){
+    console.log("You get a very good discount!")
+}
+else if 
